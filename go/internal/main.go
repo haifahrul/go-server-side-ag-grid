45c5657bc2f78a07959747f96fedd02733918f8f
A Fakhrurozi S <haifahrul@gmail.com>
2020-02-29 13:40:31 +0700

    fix bug query

--- go/internal/main.go
+++ go/internal/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -11,17 +11,17 @@ import (
 
 // Model struct
 type Model struct {
-	Athlete      string    `json="athlete"`
-	Age          int32     `json="age"`
-	Country      string    `json="country"`
-	CountryGroup *string   `json="country_group"`
-	Year         int32     `json="year"`
-	Date         time.Time `json="date"`
-	Sport        string    `json="sport"`
-	Gold         int64     `json="gold"`
-	Silver       int64     `json="silver"`
-	Bronze       int64     `json="bronze"`
-	Total        int64     `json="total"`
+	Athlete       string  `json="athlete"`
+	Age           int32   `json="age"`
+	Country       string  `json="country"`
+	Country_Group *string `json="country_group"`
+	Year          int32   `json="year"`
+	Date          string  `json="date"`
+	Sport         string  `json="sport"`
+	Gold          int64   `json="gold"`
+	Silver        int64   `json="silver"`
+	Bronze        int64   `json="bronze"`
+	Total         int64   `json="total"`
 }
 
 func main() {
@@ -34,13 +34,20 @@ func main() {
 
 	var rows []Model
 
-	qryStr := `SELECT athlete FROM olympic_winners`
+	qryStr := `SELECT * FROM olympic_winners LIMIT 1`
 	err = db.Select(&rows, qryStr)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	log.Println("FAHRUL", rows)
+
+	result, err := json.Marshal(rows)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	log.Println(string(result))
 }
 
 // ConnectSqlx connection
